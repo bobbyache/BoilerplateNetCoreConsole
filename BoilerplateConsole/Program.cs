4eb56b9e14a78ec1939f2f4109e4aace0dd42ee6
RobBl8ke <RobB@korbicom.com>
2020-08-19 12:27:15 +0200

    CommandLineParser for argument management.

--- BoilerplateConsole/Program.cs
+++ BoilerplateConsole/Program.cs
@@ -1,17 +1,40 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using CommandLine;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
 
 // Dependency Injection and Settings: https://espressocoder.com/2018/12/03/build-a-console-app-in-net-core-like-a-pro/
+// CommandLineParser: https://github.com/commandlineparser/commandline?WT.mc_id=-blog-scottha
 
 namespace BoilerplateConsole
 {
     public class Program
     {
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+        }
+
         public static void Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+                   });
+
+        var builder = new ConfigurationBuilder()
              .SetBasePath(Directory.GetCurrentDirectory())
              .AddJsonFile("appsettings.json");
             // .AddEnvironmentVariables("JRTech_");
